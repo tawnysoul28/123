c4340e182ee2b92a111767b25cb7e250f69bbdd5
Bob <tawnysoul@MacBook-Pro-Bob.2gis.local>
2019-05-26 17:22:14 +0700

    initial commit

---
+++ 123/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  123
+//
+//  Created by Bob on 26/05/2019.
+//  Copyright © 2019 Bob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
